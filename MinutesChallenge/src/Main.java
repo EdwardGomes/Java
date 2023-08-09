330ba20ee5cb8e8f53322c6cb18896560965bba8
Eduardo Gomes <edugomes514@gmail.com>
2023-08-09 14:46:49 -0300

    seconds and minutes challenge

---
+++ MinutesChallenge/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(getDurationString(65, 45));
+        System.out.println(getDurationString(3945));
+    }
+
+    public static String getDurationString (int seconds){
+        if (seconds < 0){
+            return "Invalid Value";
+        }
+        return getDurationString(seconds / 60, seconds % 60);
+    }
+
+    public static String getDurationString (int minutes, int seconds){
+
+        if (minutes < 0 || seconds < 0 || seconds > 59){
+            return "Invalid Value";
+        }
+
+        int hours = minutes / 60;
+        minutes = minutes % 60;
+
+        return hours + "h " + minutes + "m " + seconds + "s";
+    }
+}
